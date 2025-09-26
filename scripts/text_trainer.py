860a2875a518582fa41741f1c699207c65f3e37f
Herdian Apriasmoro <apriasmoro@gmail.com>
2025-09-26 23:21:51 +0700

    2025-09-26 23:21:51

--- scripts/text_trainer.py
+++ scripts/text_trainer.py
@@ -200,7 +200,7 @@ def main():
     )
     
     parser.add_argument(
-        "--reg-ratio", type=float, help="Reg ratio to use for training", default=0.9209
+        "--reg-ratio", type=float, help="Reg ratio to use for training", default=0.9589
     )
     
     args = parser.parse_args()
